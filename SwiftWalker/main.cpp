fe2c381fe55f3fc85d786a99b7160222946c1a0d
tdermendjiev <tdermendjievft@gmail.com>
2020-02-14 17:18:21 +0200

    Initial Commit

---
+++ SwiftWalker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SwiftWalker
+//
+//  Created by Teodor Dermendzhiev on 14.02.20.
+//  Copyright © 2020 Teodor Dermendzhiev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
