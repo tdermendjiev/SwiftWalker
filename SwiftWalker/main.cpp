37245c1e6c533da92507562a4561ead636621a96
tdermendjiev <tdermendjievft@gmail.com>
2020-02-21 07:57:41 +0200

    Link swift libraries and build project

--- SwiftWalker/main.cpp
+++ SwiftWalker/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include <swift/Frontend/Frontend.h>
+#include <swift/FrontendTool/FrontendTool.h>
+#include "swift/Parse/Lexer.h"
+
+using namespace swift;
+
+void anchorForGetMainExecutable() {}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+        CompilerInvocation Invocation;
+    
+        Invocation.setMainExecutablePath(
+        llvm::sys::fs::getMainExecutable(argv[0],
+        reinterpret_cast<void *>(&anchorForGetMainExecutable)));
+    
+        Invocation.getFrontendOptions().InputsAndOutputs.addInputFile(
+            "placeholder");
+        Invocation.getLangOptions().AttachCommentsToDecls = true;
+        Invocation.getLangOptions().CollectParsedToken = true;
+        Invocation.getLangOptions().BuildSyntaxTree = true;
+        CompilerInstance CI;
+        if (CI.setup(Invocation))
+          return 1;
+    
+        CI.performSema();
+        auto &ctx = CI.getASTContext();
+        auto swiftUI = ctx.getLoadedModule(ctx.getIdentifier("SwiftUI"));
+        if (!swiftUI) {
+          llvm::errs() << "error: SwiftUI module not loaded\n";
+        }
+    
+    
     return 0;
 }
